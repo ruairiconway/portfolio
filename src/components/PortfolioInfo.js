3f0d2141bd64bb3f166ee20a9a0dda11f57296b5
Ruairi Conway <hi@ruairiconway.com>
2021-05-05 18:02:30 -0400

    added icons to project page

--- src/components/PortfolioInfo.js
+++ src/components/PortfolioInfo.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
+import { StarIcon, FlagIcon, HtmlOpenIcon, HtmlCloseIcon } from '../assets/icons'
+
+const TitleWrapper = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    margin-bottom: 0.5rem;
+
+    h5 {
+        ${({ type }) => {
+            if (type === 'right') {
+                return 'margin: 0 1rem 0 0;'
+            } else if (type === 'left') {
+                return 'margin: 0 0 0 1rem;'
+            } else if (type === 'both') {
+                return 'margin: 0 1rem;'
+            }
+        }}
+    }
+`
 
 const Title = styled.h5`
-    margin-bottom: 0.5rem;
+
 `
 
 export default function PortfolioInfo({ highlights, todos, skills, ...props }) {
@@ -14,19 +33,29 @@ export default function PortfolioInfo({ highlights, todos, skills, ...props }) {
     return (
         <>
             <div>
-                <Title>Highlights</Title>
+                <TitleWrapper type='right'>
+                    <Title>Highlights</Title>
+                    <StarIcon />
+                </TitleWrapper>
                 <ul>
                     {highlightsList}
                 </ul>
             </div>
             <div>
-                <Title>Todos</Title>
+                <TitleWrapper type='left'>
+                    <FlagIcon />
+                    <Title>Todos</Title>
+                </TitleWrapper>
                 <ul>
                     {todosList}
                 </ul>
             </div>
             <div>
-                <Title>Skills</Title>
+                <TitleWrapper type='both'>
+                    <HtmlOpenIcon />
+                    <Title>Skills</Title>
+                    <HtmlCloseIcon />
+                </TitleWrapper>
                 <ul>
                     {skillsList}
                 </ul>
