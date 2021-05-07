21c4c6d7b5a124e4a88a13f0b92b6390d3efd257
Ruairi Conway <hi@ruairiconway.com>
2021-05-07 16:25:22 -0400

    implemented styling

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,26 +1,83 @@
 import React, { useContext } from 'react'
+import styled from 'styled-components'
 import { PortfolioContext } from '../contexts/PortfolioContext'
+
+const Card = styled.div`
+    
+`
+
+const PortfolioBtn = styled.button`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    width: 100%;
+    height: 200px;
+    padding: 0;
+    border: none;
+    background-image: url(${({ value }) => value.image});
+    background-position: center;
+    transition: 0.2s;
+    cursor: pointer; 
+
+    h4 {
+        max-width: 75%;
+        padding: 0.5rem 1rem;
+        text-align: right;
+        color: var(--ranColor);
+        background: #FFF;
+        transition: 0.2s;
+    }
+
+    &:hover, &:focus {
+        box-shadow: 5px 5px 0px 0px var(--ranColor);
+
+        h4 {
+            color: #FFF;
+            background: var(--ranColor);
+        }
+    }
+
+    ${({ value, portfolioActive, portfolioIndex }) => {
+        if (portfolioActive && portfolioIndex === value.id) {
+            return `
+                box-shadow: 5px 5px 0px 0px var(--ranColor);
+
+                h4 {
+                    color: #FFF;
+                    background: var(--ranColor);
+                }
+            `
+        }
+    }}
+
+    &:active {
+        box-shadow: 3px 3px 0px 0px var(--ranColor);
+
+        h4 {
+            color: #000;
+            background: var(--ranColor);
+        }
+    }
+`
 
 export default function ProjectCard({value, ...props}) {
 
-    const { setPortfolioActive, setPortfolioIndex } = useContext(PortfolioContext)
-
-    var divStyle = {
-        height: '100px',
-        width: '150px',
-        backgroundImage: `url(${value.image})`,
-        backgroundPosition: 'center',
-    }
+    const { portfolioActive, setPortfolioActive, portfolioIndex, setPortfolioIndex } = useContext(PortfolioContext)
 
     return (
-        <div>
-            <button onClick={() => {
+        <Card>
+            <PortfolioBtn
+                value={value}
+                portfolioActive={portfolioActive}
+                portfolioIndex={portfolioIndex}
+                onClick={() => {
                 setPortfolioActive(true)
                 setPortfolioIndex(value.id)
-            }} style={divStyle}>
+                }}
+            >
                 <h4>{value.name}</h4>
-            </button>
+            </PortfolioBtn>
             <p>{value.caption}</p>
-        </div>
+        </Card>
     )
 }
