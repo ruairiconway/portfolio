c9d9eb0d31a83500df061b55190bdbe06647b20d
Ruairi Conway <hi@ruairiconway.com>
2021-05-07 12:32:02 -0400

    created ContactForm component

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,54 +1,21 @@
-import React, { useState } from 'react'
-import { SendIcon } from '../assets/icons'
+import React from 'react'
+import styled from 'styled-components'
+import ContactForm from '../components/ContactForm'
 import PageCount from '../components/PageCount'
 
+const Title = styled.h3`
+    margin: 2rem 0 3rem 0;
+    color: var(--ranColor);
+    transition: 0.5s;
+`
+
 export default function ContactPage() {
-
-    const [ formName, setFormName ] = useState('')
-    function handleNameChange(e) { setFormName(e.target.value) }
-    
-    const [ formEmail, setFormEmail ] = useState('')
-    function handleEmailChange(e) { setFormEmail(e.target.value) }
-    
-    const [ formMessage, setFormMessage ] = useState('')
-    function handleMessageChange(e) { setFormMessage(e.target.value) }
-
     return (
         <>
             <PageCount>04</PageCount>
-            <h3>Contact</h3>
+            <Title>Contact</Title>
             <p>I’m always looking for new opportunities.<br/>Whether it’s freelance or full-time. Give me a shout!</p>
-            <form>
-                <input
-                    type='text'
-                    name="name"
-                    placeholder="Name"
-                    value={formName}
-                    aria-label="enter name"
-                    onChange={handleNameChange}
-                />
-                <input
-                    type='email'
-                    name="email"
-                    placeholder="Email"
-                    value={formEmail}
-                    aria-label="enter email"
-                    onChange={handleEmailChange}
-                />
-                <textarea
-                    name="message"
-                    value={formMessage}
-                    placeholder="Your message..."
-                    aria-label="enter message"
-                    onChange={handleMessageChange}
-                />
-                <SendIcon />
-                <SendIcon />
-                <button
-                    type="submit"
-                    aria-label="send message"
-                >Send</button>
-            </form>
+            <ContactForm />
         </>
     )
 }
